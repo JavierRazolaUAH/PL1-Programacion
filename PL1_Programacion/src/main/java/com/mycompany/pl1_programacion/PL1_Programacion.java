891f175ef2b543131f477ec0ba2a2e48482df629
JavierRazolaUAH <javier.razola@edu.uah.es>
2026-03-30 16:09:39 +0200

    Add files via upload

---
+++ PL1_Programacion/src/main/java/com/mycompany/pl1_programacion/PL1_Programacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pl1_programacion;
+
+/**
+ *
+ * @author javir
+ */
+public class PL1_Programacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
