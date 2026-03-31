b5ed8f4d4ddc30cfae0a554289e665bcd7bbac93
JavierRazola <javier.razola@edu.uah.es>
2026-03-31 18:33:29 +0200

    c

--- PL1_Programacion/src/main/java/Clases/CallePrincipal.java
+++ PL1_Programacion/src/main/java/Clases/CallePrincipal.java
@@ -27,27 +27,29 @@ public class CallePrincipal {
 
     // --- MÉTODOS DE LA SIMULACIÓN ---
 
-    // 1. Método para cuando el niño acaba de ser creado en el Main
-    public void inicio(Nino nino) throws InterruptedException {
+  // --- MÉTODOS DE ENTRADA Y SALIDA ---
+    public void entrar(Nino nino) throws InterruptedException {
         ninosEnCalle.put(nino);
-        // Logs.getInstance().log(nino.getNombre() + " ha iniciado su actividad en la CALLE PRINCIPAL.");
-        
-        // No hay tiempo de espera obligatorio al inicio según el enunciado, 
-        // así que el niño sale inmediatamente hacia el Sótano Byers.
+    }
+
+    public void salir(Nino nino) {
         ninosEnCalle.remove(nino);
     }
 
-    // 2. Método para el final del ciclo (Deambular)
+    // --- MÉTODOS DE ACCIÓN ---
+    public void inicio(Nino nino) throws InterruptedException {
+        entrar(nino);
+        // Como no hay tiempo de espera al inicio, sale inmediatamente
+        salir(nino);
+    }
+
     public void deambular(Nino nino) throws InterruptedException {
-        ninosEnCalle.put(nino);
-        // Logs.getInstance().log(nino.getNombre() + " está deambulando por la CALLE PRINCIPAL.");
+        entrar(nino);
         
-        // Tiempo deambulando: Aleatorio entre 3 y 5 segundos (3000 - 5000 ms)
         int tiempoDeambular = rand.nextInt(2000) + 3000; 
         Thread.sleep(tiempoDeambular);
         
-        ninosEnCalle.remove(nino);
-        // Logs.getInstance().log(nino.getNombre() + " deja de deambular y vuelve al SÓTANO BYERS.");
+        salir(nino);
     }
 
     // --- MÉTODOS GETTER (Para tu Interfaz y RMI) ---
